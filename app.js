f0602a8947bdaecbaca9d3a91549ffcc192aa901
Stoyann <stoyann.openclassrooms@gmail.com>
2022-04-26 21:16:27 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// ======================= point de terminaison ====================
+
+const express = require("express");
+const pokemons = require("./mock-pokemon");
+
+const app = express();
+// declaration du port de l'api rest
+const port = 5000;
+
+// End point
+app.get("/", (req, res) => res.send("Bienvenue sur l'api rest Pokémon!"));
+
+app.get("/api/pokemon/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+
+  res.json(pokemon);
+});
+
+// End point nombre de pokémons dans le pokédex
+app.get("/api/pokemons", (req, res) => {
+  res.send(
+    `Il y'a  ${pokemons.length} pokémons dans le pokédex, pour le moment`
+  );
+});
+
+// demarage de l'api rest sur le port 3000 et afficahge d'un message
+app.listen(port, () =>
+  console.log("Notre application Node est demarée sur le port 5000")
+);
