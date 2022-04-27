868fef71f1c4da8dd6e3c3cbc05ca0d7680d0372
Stoyann <stoyann.openclassrooms@gmail.com>
2022-04-27 19:22:14 +1100

    Ajout de l'endpoint PUT pour modifier un pokémon dans la base de données

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const pokemons = require("./mock-pokemon");
 
 const app = express();
 // declaration du port de l'api rest
-const port = 5000;
+const port = 8000;
 
 // ================================ les Middlewares ==========================
 //les middlewares sont une fonctionnalitées d'express qui effectue des tâches trés diverse
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 // End point
 app.get("/", (req, res) => res.send("Bienvenue sur l'api rest Pokémon!"));
 
-// enpoint  recuperation d'un pokemon avec  son id au format json
+// enpoint GET d'un pokemon avec  son id au format json
 app.get("/api/pokemon/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
@@ -41,14 +41,14 @@ app.get("/api/pokemon/:id", (req, res) => {
   res.json(helper.success(message, pokemon));
 });
 
-// enpoint récuperation de tous les pokemons au format json
+// enpoint GET de tous les pokemons au format json
 
 app.get("/api/pokemons", (req, res) => {
   const message = `Statut: Votre requête c’est bien passer une liste de ${pokemons.length} pokémons à été trouver.`;
   res.json(helper.success(message, pokemons));
 });
 
-// endpoint  post d'un nouveau pokémon
+// endpoint  POST d'un nouveau pokémon
 app.post("/api/pokemons", (req, res) => {
   const id = helper.getUniqueId(pokemons);
   const pokemonCreated = { ...req.body, ...{ id: id, created: new Date() } };
@@ -57,7 +57,19 @@ app.post("/api/pokemons", (req, res) => {
   res.json(helper.success(message, pokemonCreated));
 });
 
+// endpoint PUT modifier un pokémon
+app.put("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemonUpdated = { ...req.body, id: id };
+  pokemons.map((pokemon) => {
+    return pokemon.id === id ? pokemonUpdated : pokemon;
+  });
+
+  const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié.`;
+  res.json(helper.success(message, pokemonUpdated));
+});
+
 // demarage de l'api rest sur le port 3000 et afficahge d'un message
 app.listen(port, () =>
-  console.log("Notre application Node est demarée sur le port 5000")
+  console.log("Notre application Node est demarée sur le port 8080")
 );
