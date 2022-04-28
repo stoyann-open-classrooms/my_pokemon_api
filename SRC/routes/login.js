af74fc26b886d97587e43d7736938610ecc9e803
Stoyann <stoyann.openclassrooms@gmail.com>
2022-04-29 00:16:33 +1100

    gestion des erreurs liée au login

--- SRC/routes/login.js
+++ SRC/routes/login.js
@@ -3,15 +3,27 @@ const bcrypt = require("bcrypt");
 
 module.exports = (app) => {
   app.post("/api/login", (req, res) => {
-    User.findOne({ where: { username: req.body.username } }).then((user) => {
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((isPasswordValid) => {
-          if (isPasswordValid) {
+    User.findOne({ where: { username: req.body.username } })
+      .then((user) => {
+        if (!user) {
+          const message = `L'uttilisateur demandé n'éxiste pas `;
+          res.status(404).json({ message });
+        }
+
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((isPasswordValid) => {
+            if (!isPasswordValid) {
+              const message = `Le mot de passe est incorect`;
+              res.status(401).json({ message });
+            }
             const message = `L'utilisateur a été connecté avec succès`;
             return res.json({ message, data: user });
-          }
-        });
-    });
+          });
+      })
+      .catch((error) => {
+        const message = `L'utilisateur n'a pas pu être connecté. Reesayer dans quelque instants.`;
+        return res.json({ message, data: error });
+      });
   });
 };
