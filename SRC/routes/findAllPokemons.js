7c340002f527a030347dcc11b2fb444f8be77c8a
Stoyann <stoyann.openclassrooms@gmail.com>
2022-04-28 20:47:27 +1100

    ajout de la fonctionnalités de recherche par no

--- SRC/routes/findAllPokemons.js
+++ SRC/routes/findAllPokemons.js
@@ -1,15 +1,24 @@
 const { Pokemon } = require("../db/sequelize");
+const pokemon = require("../models/pokemon");
 
 module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
-    Pokemon.findAll()
-      .then((pokemons) => {
-        const message = "La liste des pokémons a bien été récupérée.";
+    if (req.query.name) {
+      const name = req.query.name;
+      return Pokemon.findAll({ where: { name: name } }).then((pokemons) => {
+        const message = `Il y'a ${pokemons.length} pokémons qui correspondent au terme de recherches ${name}`;
         res.json({ message, data: pokemons });
-      })
-      .catch((error) => {
-        const message = `La liste de pokémons n'a pas pu être récupérée. Réesayer dans quelques instants.`;
-        res.status(500).json({ message, data: error });
       });
+    } else {
+      Pokemon.findAll()
+        .then((pokemons) => {
+          const message = "La liste des pokémons a bien été récupérée.";
+          res.json({ message, data: pokemons });
+        })
+        .catch((error) => {
+          const message = `La liste de pokémons n'a pas pu être récupérée. Réesayer dans quelques instants.`;
+          res.status(500).json({ message, data: error });
+        });
+    }
   });
 };
